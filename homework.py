92b9975cbb6151c7096e8c12a8c6c011c566dba4
nu-shtosh <i9858955749@ya.ru>
2021-08-25 17:32:16 +0300

    poka hz)

--- homework.py
+++ homework.py
@@ -1,25 +1,40 @@
+import logging
 import os
 import time
+from logging.handlers import RotatingFileHandler
+
 import requests
 import telegram
 from dotenv import load_dotenv
 
-
 load_dotenv()
+
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log', filemode='w',
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler('my_logger.log',
+                              maxBytes=50000000,
+                              backupCount=5)
+logger.addHandler(handler)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    homework_statuses = homework['status']
+    if homework_statuses == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -27,24 +42,29 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    homework_statuses = requests.get(URL, headers=headers, params=payload)
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    current_timestamp = int(time.time())  # Начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
-            ...
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
+            logger.debug('Отслеживание статуса запущено')
+            logger.info('Бот отправил сообщение')
+            time.sleep(5 * 60)
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+            error_message = f'Бот упал с ошибкой: {e}'
+            logger.error(error_message)
+            bot.send_message(chat_id=CHAT_ID, text=error_message)
             time.sleep(5)
 
 

4f54c237178fea5cbc0a967b30a7ff097db16e52
nu-shtosh <i9858955749@ya.ru>
2021-09-01 00:31:15 +0300

    ispravlenia

--- homework.py
+++ homework.py
@@ -10,15 +10,6 @@ from dotenv import load_dotenv
 load_dotenv()
 os.path.expanduser('~')
 
-PRAKTIKUM_TOKEN = os.environ['PRAKTIKUM_TOKEN']
-TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
-CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
-
-TG_BOT = telegram.Bot(token=TELEGRAM_TOKEN)
-
-YAP_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-AUTH_YAP_TOKEN = f'OAuth {PRAKTIKUM_TOKEN}'
-
 logging.basicConfig(
     level=logging.DEBUG,
     filename='main.log', filemode='w',
@@ -26,20 +17,35 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
+PRAKTIKUM_TOKEN = os.environ['PRAKTIKUM_TOKEN']
+if PRAKTIKUM_TOKEN is None:
+    LOGGER.debug('Токен отсутсвует!')
+    raise ValueError('Токен отсутсвует!')
+
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+
+TG_BOT = telegram.Bot(token=TELEGRAM_TOKEN)
+YAP_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+AUTH_YAP_TOKEN = f'OAuth {PRAKTIKUM_TOKEN}'
+HEADERS = {'Authorization': AUTH_YAP_TOKEN}
+
+STATUS_ERROR = 'Ошибка статуса!'
+NONE_ERROR = 'STATUS OR HOMEWORK IS NONE'
+STATUSES = {
+    'rejected': 'К сожалению, в работе нашлись ошибки.',
+    'approved': 'Ревьюеру всё понравилось, работа зачтена!',
+    'reviewing': 'Вашу работу проверяют.',
+}
+
 
 def parse_homework_status(homework):
     HW_NAME = homework.get('homework_name')
     HW_STATUS = homework.get('status')
-    GOOD_VERDICT = 'Ревьюеру всё понравилось, работа зачтена!'
-    BAD_VERDICT = 'К сожалению, в работе нашлись ошибки.'
-    REVIEW_VERDICT = 'Вашу работу проверили.'
-    STATUS_ERROR = 'Ошибка статуса!'
-    STATUSES = {
-        'rejected': BAD_VERDICT,
-        'approved': GOOD_VERDICT,
-        'reviewing': REVIEW_VERDICT,
-    }
-    print(homework)
+
+    if HW_NAME is None or HW_STATUS is None:
+        raise ValueError(NONE_ERROR)
+
     REVIEWING = f'У вас проверили работу "{HW_NAME}"!\n\n{STATUSES[HW_STATUS]}'
     if HW_STATUS in STATUSES.keys():
         return REVIEWING.format(HW_NAME, STATUSES[HW_STATUS])
@@ -50,11 +56,10 @@ def parse_homework_status(homework):
 def get_homeworks(current_timestamp):
     if current_timestamp is None:
         current_timestamp = int(time.time())
-    HEADERS = {'Authorization': AUTH_YAP_TOKEN}
     PAYLOAD = {'from_date': current_timestamp}
-    URLHEADPAYLOAD = dict(url=YAP_URL, headers=HEADERS, params=PAYLOAD)
-    RESPONSE = requests.get(**URLHEADPAYLOAD)
-    HW_STATUS = RESPONSE.json()
+    UrlHeadPayload = dict(url=YAP_URL, headers=HEADERS, params=PAYLOAD)
+    response = requests.get(**UrlHeadPayload)
+    HW_STATUS = response.json()
     if HW_STATUS.get('error') or HW_STATUS.get('code'):
         error = HW_STATUS.get('error')
         code = HW_STATUS.get('code')
@@ -66,6 +71,7 @@ def send_message(message):
     try:
         TG_BOT.send_message(chat_id=CHAT_ID, text=message)
         LOGGER.info('Отправлено сообщение в чат!')
+
     except Exception as SEND_ERROR:
         SEND_ERROR = 'Во время отправки возникла ошибка!'
         LOGGER.error(SEND_ERROR)
@@ -74,8 +80,7 @@ def send_message(message):
 
 def main():
     current_timestamp = int(time.time())
-    INTERVAL = time.sleep(5 * 60)
-    SLEEP = time.sleep(5)
+    INTERVAL = 5 * 60
     while True:
         try:
             LOGGER.debug('Бот был запущен!')
@@ -84,12 +89,12 @@ def main():
             LOGGER.info('Бот отправил сообщение!')
             for homework in homeworks:
                 send_message(parse_homework_status(homework))
-            INTERVAL
+            time.sleep(INTERVAL)
         except Exception as e:
-            error_message = f'Бот упал с ошибкой: {e}!'
-            LOGGER.error(error_message)
-            TG_BOT.send_message(chat_id=CHAT_ID, text=error_message)
-            SLEEP
+            ERROR_MESSAGE = f'Бот упал с ошибкой: {e}!'
+            LOGGER.error(ERROR_MESSAGE)
+            TG_BOT.send_message(chat_id=CHAT_ID, text=ERROR_MESSAGE)
+            time.sleep(5)
 
 
 if __name__ == '__main__':
